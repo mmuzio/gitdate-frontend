7f0105e5661e7104855087290e519f9766d0fcf8
mmuzio <mmuzio@hmc.edu>
2020-05-24 12:20:15 -0400

    Document Headers Service

--- projects/angular-ngrx-material-starter/src/app/features/headers/headers.service.ts
+++ projects/angular-ngrx-material-starter/src/app/features/headers/headers.service.ts
@@ -2,11 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpHeaders } from '@angular/common/http';
 import { ACCESS_TOKEN } from '../../../environments/environment';
 
+/**
+ * HeadersServices adds necessary headers to HTTP requests
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class HeadersService {
 
+  /**
+   * getHeaders returns HttpHeaders needed to make authenticated requests to GitDate API
+   */
   public getHeaders(): HttpHeaders {
     let headers = new HttpHeaders().set('Content-Type', 'application/json');
     if (localStorage.getItem(ACCESS_TOKEN)) {
@@ -15,6 +21,9 @@ export class HeadersService {
     return headers;
   }
 
+  /**
+   * Inject necessary services
+   */
   constructor() {}
 
 }
